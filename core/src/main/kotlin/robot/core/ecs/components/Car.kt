6407caafcc0a139f519732616d4c4fa738a4c933
Tommie Nygren <tommie.nygren@gmail.com>
2022-09-20 11:25:39 +0200

    Added a control vector to the car

--- core/src/main/kotlin/robot/core/ecs/components/Car.kt
+++ core/src/main/kotlin/robot/core/ecs/components/Car.kt
@@ -2,8 +2,11 @@ package robot.core.ecs.components
 
 import com.badlogic.ashley.core.Component
 import com.badlogic.gdx.utils.Pool.Poolable
+import ktx.math.vec2
 
 class Car : Component, Poolable {
+    val controlVector = vec2()
     override fun reset() {
+        controlVector.setZero()
     }
 }
